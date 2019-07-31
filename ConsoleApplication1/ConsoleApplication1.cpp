9a69787f1f1274f0f6ee408c0f99d3ac3a9f5131
vmlab19wa <vmlabwata@gmail.com>
2019-07-31 14:43:07 +0900

    hello

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,10 @@
+﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
